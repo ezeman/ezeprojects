db341ee80e0ebe81764ad20983438ee3ad4a9a79
Narong Ezeman <mrnok@yahoo.com>
2020-06-29 16:21:25 +0700

    second commit

--- testxml.py
+++ testxml.py
@@ -6,6 +6,6 @@ myroot = mytree.getroot()
 #print(myroot)
 
 for item in myroot.iterfind('text'):
-    print(item.findtext('id') + "---" + item.findtext('value') )
+    print(item.findtext('id')[0:4] + "--" + item.findtext('id')[4:len(item.findtext('id'))] + "--" + item.findtext('value') )
 
 
